d97cc5c24ddea238a4c13431a548bf678751b6f8
EricLEE0828 <ericlee050828@gmail.com>
2025-05-16 02:48:10 +0800

    Docs: Add workflow files

--- setup.py
+++ setup.py
@@ -3,14 +3,11 @@
 import os
 from setuptools import setup, find_packages
 
-# 讀取長描述
+# read long description
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-# 讀取版本
-version = "0.1.1"
-
-# 設置
+# setup
 setup(
     packages=find_packages(exclude=["tests*", "examples*"]),
 )
